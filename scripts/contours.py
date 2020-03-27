65b6aded6d7f892269e755b5ff16b3c0e4031878
Mohammad Mahdi Shirazi <mhmmdshirazi@gmail.com>
2020-03-27 20:12:37 +0100

    Tune threshold value

--- scripts/contours.py
+++ scripts/contours.py
@@ -5,7 +5,7 @@ import cv2 as cv
 # Finding the letter contour in the picture
 def find_contour(img):
     imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, thresh = cv.threshold(imgray, 127, 255, 0)
+    ret, thresh = cv.threshold(imgray, 60, 255, 0)
     contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     # Return None if there is no contour in picture
     if len(contours) == 0:
@@ -34,6 +34,11 @@ def find_HSU(img, h_cnt, s_cnt, u_cnt):
     u_diff = find_diff(contour, u_cnt)
     min_diff = min([h_diff, s_diff, u_diff])
     diff_arg = np.argmin([h_diff, s_diff, u_diff])
+    # debug
+    # cv.drawContours(img,[h_cnt],0,(0,255,0),1)
+    # cv.drawContours(img, [contour], 0, (0, 0, 255), 1)
+    # cv.imshow('c',img)
+    cv.waitKey()
     if min_diff > 0.4:
         return None, None
     if diff_arg == 0:
