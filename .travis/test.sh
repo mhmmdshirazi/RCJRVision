b1d735606f1cedc18d76fd4e6310baca10d2dbe3
Mohammad Mahdi Shirazi <mhmmdshirazi@gmail.com>
2020-04-01 01:18:58 +0200

    Add a simple test.sh that runs the main file for travis

---
+++ .travis/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  set -e
+  set -x
+  IS_IN_TRAVIS=false
+  PYTHON_COMMAND=python
+  
+  if [ "$TRAVIS_OS_NAME" == "osx" ]
+  then
+	PYTHON_COMMAND=python3
+  fi
+  $PYTHON_COMMAND -m main.py
